cc0af29b821760cc75c1869b59273d9bc88bcb85
saloi sbai <saloi.benoum@gmail.com>
2024-01-10 22:59:06 +0100

    refacto index.php

---
+++ oeuvres.php
@@ -0,0 +1,115 @@
+<?php
+$oeuvres = [
+    [
+        'id' => 1,
+        'titre' => 'Dodomu',
+        'description_courte' => 'Mia Tozerski',
+         'description_complete' => "Mia Tozerski est une artiste peintre ukrainienne réfugiée de la guerre. Sur cette œuvre, Dodomu (\"domicile\" en ukrainien), elle nous montre la tristesse du peuple ukrainien qu'elle partage, ayant elle-même dû quitter son foyer. L'œuvre évoque le drapeau liquéfié d'une Ukraine en souffrance, pleurant la mort de ses compatriotes. Ce travail chargé d'émotion est le symbole d'un événement qui marquera l'Histoire. Cette peinture à l'acrylique rayonne grâce à son fond lisse et ses mélanges de couleurs éclatantes.",
+        'image' => 'img/clark-van-der-beken.png'
+    ],
+    [
+        'id' => 2,
+        'titre' => "Aashaaheen Baadal",
+        'description_courte' => 'Anaisha Devi',
+        'description_complete' => 'Sur cette oeuvre conceptuelle à la fois organique, minérale et liquide, Anaisha Devi nous transporte dans un nuage noir envoûtant. Un sombre tableau qui, par son verni éclatant, rayonne tel un marbre poli. Une oeuvre à la cohérence transcendantale, exécutée à la perfection',
+        'image' => 'img/pawel-czerwinski-3.png'
+
+
+    ],
+    [
+        'id' => 3,
+        'titre' => " Nightlife Traffic",
+        'description_courte' => 'Andrew Forsythe',
+        'description_complete' => 'Quisque accumsan ultrices ligula vestibulum posuere. Aliquam feugiat ligula eget massa blandit condimentum. Morbi volutpat erat luctus suscipit pellentesque. Quisque cursus tempor nibh at sollicitudin. Sed blandit libero velit. Etiam tincidunt facilisis mollis. Ut mollis nunc sit amet lacinia luctus. Suspendisse volutpat enim semper arcu rutrum, et iaculis risus interdum. Duis at libero.',
+        'image' => 'img/dan-cristian-padure.png'
+
+    ],
+    [
+        'id' => 4,
+        'titre' => " ",
+        'description_courte' => '',
+        'description_complete' => '',
+        'image' => ''
+    ],
+    [
+        'id' => 5,
+        'titre' => " ",
+        'description_courte' => '',
+        'description_complete' => '',
+        'image' => ''
+    ],
+    [
+        'id' => 6,
+        'titre' => " ",
+        'description_courte' => '',
+        'description_complete' => '',
+        'image' => ''
+    ],
+    [
+        'id' => 7,
+        'titre' => " ",
+        'description_courte' => '',
+        'description_complete' => '',
+        'image' => ''
+    ],
+    [
+        'id' => 8,
+        'titre' => " ",
+        'description_courte' => '',
+        'description_complete' => '',
+        'image' => ''
+    ],
+    [
+        'id' => 9,
+        'titre' => " ",
+        'description_courte' => '',
+        'description_complete' => '',
+        'image' => ''
+    ],
+    [
+        'id' => 10,
+        'titre' => " ",
+        'description_courte' => '',
+        'description_complete' => '',
+        'image' => ''
+    ],
+    [
+        'id' => 11,
+        'titre' => " ",
+        'description_courte' => '',
+        'description_complete' => '',
+        'image' => ''
+    ],
+    [
+        'id' => 12,
+        'titre' => " ",
+        'description_courte' => '',
+        'description_complete' => '',
+        'image' => ''
+    ],
+    [
+        'id' => 13,
+        'titre' => " ",
+        'description_courte' => '',
+        'description_complete' => '',
+        'image' => ''
+    ],
+    [
+        'id' => 14,
+        'titre' => " ",
+        'description_courte' => '',
+        'description_complete' => '',
+        'image' => ''
+    ],
+    [
+        'id' => 15,
+        'titre' => " ",
+        'description_courte' => '',
+        'description_complete' => '',
+        'image' => ''
+    ]
+
+    
+
+];
+
